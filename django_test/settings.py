6d038b1dd1d3fa5667687d8ef9e26336dd9b2c3f
Tu Nguyen <margonnguyen@gmail.com>
2019-07-08 17:22:44 -0700

    added requirement.txt

--- django_test/settings.py
+++ django_test/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') #project pased directory
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') #project pased directory
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK ='bootstrap4'
